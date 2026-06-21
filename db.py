329f690c0ee156711c1facec41dc545946b3c2d0
Ghulam jillani <ghulamjilaani005@gmail.com>
2026-06-22 00:46:45 +0500

    Update db.py

--- db.py
+++ db.py
@@ -2,7 +2,13 @@ import os
 import psycopg2
 import psycopg2.extras
 
+
+class DictConnection(psycopg2.extensions.connection):
+    def cursor(self, *args, **kwargs):
+        kwargs.pop("dictionary", None)
+        kwargs.setdefault("cursor_factory", psycopg2.extras.RealDictCursor)
+        return super().cursor(*args, **kwargs)
+
+
 def get_db_connection():
-    conn = psycopg2.connect(os.getenv("DATABASE_URL"))
-    conn.cursor_factory = psycopg2.extras.RealDictCursor
-    return conn
+    return psycopg2.connect(os.getenv("DATABASE_URL"), connection_factory=DictConnection)
